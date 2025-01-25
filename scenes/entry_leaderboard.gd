5dd7eebe8c6580809c6cd2fdec9b25e133f6a5b5
fjortegan <fjortegan@gmail.com>
2025-01-25 15:52:05 +0100

    leaderboard tab fixed

--- scenes/entry_leaderboard.gd
+++ scenes/entry_leaderboard.gd
@@ -1,15 +1,15 @@
 extends Label
 
 func _set_pos(pos: int) -> void:
-  text = text.replace("{pos}", str(pos + 1))
+	text = text.replace("{pos}", str(pos + 1))
 
 func _set_username(username: String) -> void:
-  text = text.replace("{username}", username)
+	text = text.replace("{username}", username)
 
 func _set_score(score: int) -> void:
-  text = text.replace("{score}", str(int(score)))
+	text = text.replace("{score}", str(int(score)))
 
 func set_data(pos: int, username: String, score: int) -> void:
-  _set_pos(pos)
-  _set_username(username)
-  _set_score(score)
+	_set_pos(pos)
+	_set_username(username)
+	_set_score(score)
