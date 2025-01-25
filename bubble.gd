fd5c67ce5ecc41bfc6adcadad0f8000012263b5e
fjortegan <fjortegan@gmail.com>
2025-01-25 12:13:27 +0100

    bubble division

--- bubble.gd
+++ bubble.gd
@@ -1,5 +1,7 @@
 extends RigidBody2D
 
+@export var next_bubble: PackedScene
+@export var next_bubble_count: int
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,4 +15,13 @@ func _process(delta: float) -> void:
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.is_in_group("weapon"):
+		area.queue_free()
+		Events.shoot_finished.emit()
+		if not next_bubble:
+			next_bubble = (load(scene_file_path) as PackedScene)
+		for i in range(next_bubble_count):
+			var nb = next_bubble.instantiate()
+			nb.global_position = global_position
+			nb.linear_velocity = linear_velocity + Vector2(50, 100)  - i * Vector2(100, 200)
+			get_tree().root.add_child(nb)
 		queue_free()
