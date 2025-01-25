6ee4ba5a7f504c448f1ee69a78146f3b91cd021b
fjortegan <fjortegan@gmail.com>
2025-01-25 19:17:46 +0100

    refactoring

--- scenes/character_body_2d.gd
+++ scenes/character_body_2d.gd
@@ -27,6 +27,8 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("escape"):
 		get_tree().quit()
 
+	var direction := Input.get_axis("ui_left", "ui_right")
+
 	if Input.is_action_just_pressed("ui_accept") and not shooting:
 		sprite.play("shoot")
 		shooting = true
@@ -34,20 +36,18 @@ func _physics_process(delta: float) -> void:
 		var shoot = weapon.instantiate()
 		get_tree().root.add_child(shoot)
 		shoot.global_position = global_position + weapon_offset
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
-	move_and_slide()
+		if sprite.animation != "shoot":
+			if direction:
+				velocity.x = direction * SPEED
+			else:
+				velocity.x = move_toward(velocity.x, 0, SPEED)	
+		move_and_slide()
 
 	if velocity.x != 0:
 		sprite.flip_h = velocity.x<0 
-		sprite.play("default")
+		if sprite.animation != "shoot":
+			sprite.play("default")
 	else:
 		if sprite.animation == "default":
 			sprite.stop()
