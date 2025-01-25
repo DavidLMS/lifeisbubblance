d69959576d5903301a29448871c5019afa2893b0
AlbertoLp84 <albertolp84@gmail.com>
2025-01-25 14:35:28 +0100

    Main, fuente
    Escena main y fuente añadida

---
+++ main_menu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+@onready var play_button: Button = $MarginContainer/VBoxContainer/PlayButton
+
+func _ready() -> void:
+	play_button.grab_focus()
+
+func _on_play_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/test scene.tscn")
+
+
+func _on_records_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
