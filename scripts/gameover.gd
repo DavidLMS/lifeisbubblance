75e6f2e202d7f1f207d0db90d05b1217480b3be5
AlbertoLp84 <albertolp84@gmail.com>
2025-01-26 11:23:43 +0100

    GameOver navigation
    Control the navigation of the game over scene UI with the keyboard

--- scripts/gameover.gd
+++ scripts/gameover.gd
@@ -2,10 +2,12 @@ extends Node2D
 
 var entry_scene = preload("res://scenes/entry_leaderboard.tscn")
 
-@onready var username: TextEdit = %Username
+@onready var username: LineEdit = $UI/MarginContainer/VBoxContainer/LineEdit
+#@onready var username: TextEdit = %Username
 
 func _ready() -> void:
 	%ScoreLabel.text = "SCORE: " + str(Global.score)
+	username.grab_focus()
 
 func _on_submit_pressed() -> void:
 	var username = UnicodeNormalizer.normalize(username.text)
